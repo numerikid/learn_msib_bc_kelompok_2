42ccf15776dc976ddaebd383409df1e4deb035db
rina <61769346+numerikid@users.noreply.github.com>
2023-09-15 07:37:56 +0700

    Add files via upload

---
+++ task_1/no_1.js
@@ -0,0 +1,20 @@
+// Mengimpor modul untuk mengambil input dari user
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+
+// Menggunakan readline untuk mengambil input dari user
+  readline.question('Masukkan angka pertama: ', (num1) => {
+    readline.question('Masukkan angka kedua: ', (num2) => {
+      const hasil = parseInt(num1) * parseInt(num2)
+
+      // Menampilkan hasil perkalian
+      console.log(`Hasil perkalian: ${hasil}`)
+
+      // Menutup interface readline
+      readline.close()
+    })
+  })
+  
