bb221c54ef2a70ef224e29537e56a101354940d9
rina <61769346+numerikid@users.noreply.github.com>
2023-09-16 11:51:41 +0700

    commint git ignore

--- task_1/no_1.js
+++ task_1/no_1.js
@@ -1,20 +1,19 @@
 // Mengimpor modul untuk mengambil input dari user
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+  input: process.stdin,
+  output: process.stdout
+})
 
 
 // Menggunakan readline untuk mengambil input dari user
-  readline.question('Masukkan angka pertama: ', (num1) => {
-    readline.question('Masukkan angka kedua: ', (num2) => {
-      const hasil = parseInt(num1) * parseInt(num2)
+readline.question('Masukkan angka pertama: ', (num1) => {
+  readline.question('Masukkan angka kedua: ', (num2) => {
+    const hasil = parseInt(num1) * parseInt(num2)
 
-      // Menampilkan hasil perkalian
-      console.log(`Hasil perkalian: ${hasil}`)
+    // Menampilkan hasil perkalian
+    console.log(`Hasil perkalian: ${hasil}`)
 
-      // Menutup interface readline
-      readline.close()
-    })
+    // Menutup interface readline
+    readline.close()
   })
-  
+})
